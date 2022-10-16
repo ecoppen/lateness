01ec595f519206173d470c4d908eab3a81048cf0
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-10-16 20:10:06 +0100

    Create config.py

---
+++ lateness/core/config.py
@@ -0,0 +1,42 @@
+import json
+from enum import Enum
+from typing import Dict, Optional
+
+from pydantic import BaseModel, Extra, Field, IPvAnyAddress, ValidationError, validator
+
+
+class Databases(Enum):
+    POSTGRES = "postgres"
+    SQLITE = "sqlite"
+
+
+class Database(BaseModel, use_enum_values=True, extra=Extra.forbid):  # type: ignore
+    engine: str = Databases.SQLITE  # type: ignore
+    username: Optional[str]
+    password: Optional[str]
+    host: Optional[IPvAnyAddress] = IPvAnyAddress.validate("127.0.0.1")  # type: ignore
+    port: Optional[int] = Field(5432, ge=1, le=65535)
+    name: str = "db_lateness"
+
+
+class Config(BaseModel, extra=Extra.forbid):  # type: ignore
+    database: Database
+    api_get: IPvAnyAddress = IPvAnyAddress.validate("10.192.41.212")  # type: ignore
+
+
+def load_config(path):
+    if not path.is_file():
+        raise ValueError(f"{path} does not exist")
+    else:
+        f = open(path)
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError as exc:
+            raise ValueError(
+                f"ERROR: Invalid JSON: {exc.msg}, line {exc.lineno}, column {exc.colno}"
+            )
+
+        try:
+            return Config(**data)
+        except ValidationError as e:
+            raise ValueError(f"{e}")
