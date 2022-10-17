fec316a280d391724c08f5d64558840311831778
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-10-17 08:41:13 +0100

    Create database.py

---
+++ lateness/models/database.py
@@ -0,0 +1,47 @@
+import logging
+import re
+from datetime import datetime
+
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from lateness.core.config import Database as Database_
+
+log = logging.getLogger(__name__)
+
+
+class Database:
+    def __init__(self, config: Database_) -> None:
+        if config.engine == "postgres":
+            engine_string = f"{config.username}:{config.password}@{config.host}:{config.port}/{config.name}"
+            self.engine = db.create_engine("postgresql+psycopg2://" + engine_string)
+        elif config.engine == "sqlite":
+            self.engine = db.create_engine(
+                "sqlite:///" + config.name + ".db?check_same_thread=false"
+            )
+        else:
+            raise Exception(f"{config.engine} setup has not been defined")
+
+        log.info(f"{config.engine} loaded")
+
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+        self.Base = declarative_base()
+
+        class Lateness(self.Base):  # type: ignore
+            __tablename__ = "lateness_log"
+
+            id = db.Column(db.Integer, primary_key=True)
+            upn = db.Column(db.String)
+            reason = db.Column(db.String)
+            timestamp = db.Column(
+                db.DateTime, default=datetime.now, onupdate=datetime.now
+            )
+
+        self.Base.metadata.create_all(self.engine)  # type: ignore
+        log.info("database tables loaded")
+
+    def get_table_object(self, table_name: str):
+        self.Base.metadata.reflect(self.engine)  # type: ignore
+        return self.Base.metadata.tables.get(table_name)  # type: ignore
