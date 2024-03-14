f3dcf486f4928fa2f9ef8e15742fe4f04a44c7d5
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2024-03-14 16:08:58 +0000

    Update config.py

--- lateness/core/config.py
+++ lateness/core/config.py
@@ -3,7 +3,8 @@ from datetime import time
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel, Extra, Field, HttpUrl, ValidationError
+from pydantic import BaseModel, Extra, Field, ValidationError
+from pydantic.networks import IPvAnyAddress
 
 
 class Databases(Enum):
@@ -15,14 +16,14 @@ class Database(BaseModel, use_enum_values=True, extra=Extra.forbid):  # type: ig
     engine: str = Databases.SQLITE  # type: ignore
     username: Optional[str]
     password: Optional[str]
-    host: Optional[HttpUrl] = "127.0.0.1"  # type: ignore
+    host: Optional[IPvAnyAddress] = "127.0.0.1"  # type: ignore
     port: Optional[int] = Field(5432, ge=1, le=65535)
     name: str = "db_lateness"
 
 
 class Config(BaseModel, extra=Extra.forbid):  # type: ignore
     database: Database
-    api_get: HttpUrl = "10.192.41.212"  # type: ignore
+    api_get: IPvAnyAddress = "10.192.41.212"  # type: ignore
     start_time: time = time(hour=8, minute=40)
     end_time: time = time(hour=14, minute=40)
 
